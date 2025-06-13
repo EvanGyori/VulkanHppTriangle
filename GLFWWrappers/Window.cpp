12510ea1299b8a3e393b35ebaa68c1cb868fbe44
Evan Gyori <evangyori@gmail.com>
2025-06-13 13:24:21 -0700

    Fullscreen using native resolution and refresh rate

--- GLFWWrappers/Window.cpp
+++ GLFWWrappers/Window.cpp
@@ -9,8 +9,15 @@ Window::Window(const char* title, int width, int height)
 {
     GLFWManager::init();
 
+    const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+    glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+    glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+    glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+    glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    handle = glfwCreateWindow(width, height, title, /* glfwGetPrimaryMonitor() */ nullptr, nullptr);
+    handle = glfwCreateWindow(mode->width, mode->height, title, glfwGetPrimaryMonitor(), nullptr);
     if (handle == nullptr) {
 	throw std::runtime_error("GLFW failed to create a window");
     }
